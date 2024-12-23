f27bb289a2326c893c5d179a4e18084fb6f263d2
xs <2359035384@qq.com>
2024-12-23 15:23:35 +0800

    不会啊，会不了一点

---
+++ algorithm/leetcode/simulation/855.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+/**
+ * @author: LeetCode Official
+ */
+struct Comp {
+    /**
+     * 返回true表明: p1 优先级低于 p2
+     */
+    bool operator()(const pair<int, int> &p1, const pair<int, int> &p2) {
+        int d1 = p1.second - p1.first, d2 = p2.second - p2.first;
+        return d1 / 2 < d2 / 2 || (d1 / 2 == d2 / 2 && p1.first > p2.first);
+    }
+};
+
+class ExamRoom {
+private:
+    int n;
+    set<int> seats;  // 数据有序
+    // 容器适配器
+    priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> pq;
+
+public:
+    ExamRoom(int n) : n(n) {
+        
+    }
+
+    int seat() {
+        if (seats.empty()) {
+            seats.insert(0);
+            return 0;
+        }
+        // 最左边学生和最左边座位的距离，最右边学生和最右边座位的距离
+        int left = *seats.begin() - 0, right = n - 1 - *seats.rbegin();
+        while (seats.size() >= 2) {
+            auto p = pq.top();
+            if (seats.count(p.first) > 0 && seats.count(p.second) > 0 && 
+                *next(seats.find(p.first)) == p.second) { // 不属于延迟删除的区间
+                int d = p.second - p.first;
+                if (d / 2 < right || d / 2 <= left) { // 最左或最右的座位更优
+                    break;
+                }
+                pq.pop();
+                pq.push({p.first, p.first + d / 2});
+                pq.push({p.first + d / 2, p.second});
+                seats.insert(p.first + d / 2);
+                return p.first + d / 2;
+            }
+            pq.pop(); // leave 函数中延迟删除的区间在此时删除
+        }
+        if (right > left) { // 最右的位置更优
+            pq.push({*seats.rbegin(), n - 1});
+            seats.insert(n - 1);
+            return n - 1;
+        } else {
+            pq.push({0, *seats.begin()});
+            seats.insert(0);
+            return 0;
+        }
+    }
+
+    void leave(int p) {
+        if (p != *seats.begin() && p != *seats.rbegin()) {
+            auto it = seats.find(p);
+            pq.push({*prev(it), *next(it)});
+        }
+        seats.erase(p);
+    }
+};
