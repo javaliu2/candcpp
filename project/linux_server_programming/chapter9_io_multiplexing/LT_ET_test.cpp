522dfea03afa09642530a1cdcba69164ba728d50
javaliu2 <2359035384@qq.com>
2025-01-04 12:52:52 +0800

    xs's ubuntu laptop push

---
+++ project/linux_server_programming/chapter9_io_multiplexing/LT_ET_test.cpp
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#define MAX_EVENT_NUMBER 1024
+#define BUFFER_SIZE 10
+/* set fd non-block */
+int setnonblocking(int fd){
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option; 
+}
+/* register EPOLLIN of fd to kernel event table pointered by epollfd
+ * parameter enable_et decide set or notset ET mode of fd
+ */
+void addfd(int epollfd, int fd, bool enable_et) {
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if (enable_et) {
+        event.events |= EPOLLET;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
