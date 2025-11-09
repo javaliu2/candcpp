695a77ea61928974f37f1b32de7b299491d88682
javaliu2 <2359035384@qq.com>
2025-11-09 11:21:38 +0800

    熟悉了解了监听socket和通信socket的阻塞和非阻塞模式。阻塞就是程序卡主不动，直到有事件发生，比如客户端连接到来，或者通信socket可读。非阻塞就是通过返回-1并且设置状态码的方式告知程序员，程序员需要针对不同情况做出逻辑判断和对应的处理

---
+++ project/30days_make_cpp_server/3_epoll/server_block.cpp
@@ -0,0 +1,49 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "util.h"
+#include <chrono>
+#include <string>
+#include <sys/epoll.h>
+#include <fcntl.h>
+
+
+int main () {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == -1, "socket create error");
+
+    // 设置端口重用
+    int opt = 1;
+    int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    errif(ret == -1, "set socket port reuse fail");
+
+    struct sockaddr_in sock_addr;
+    bzero(&sock_addr, sizeof(sock_addr));
+    sock_addr.sin_family = AF_INET;
+    sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sock_addr.sin_port = htons(8888);
+
+    ret = bind(sockfd, (sockaddr*)&sock_addr, sizeof(sock_addr));
+    errif(ret == -1, "socket bind error");
+
+    ret = listen(sockfd, SOMAXCONN);
+    errif(ret == -1, "socket listen error");
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    bzero(&client_addr, client_addr_len);
+
+    printf("waiting for client connnect\n");
+    int client_socket_fd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
+    errif(client_socket_fd == -1, "socket accept error");
+
+    printf("client fd: %d\n", client_socket_fd);
+    char buf[1024];
+    int n = read(client_socket_fd, buf, sizeof(buf));
+    printf("read done.\n");
+    close(sockfd);
+    return 0;
+}
